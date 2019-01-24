82f7173361e52b5143923169b7c200df2f53f8fe
llc <www.llc@qq.com>
2019-01-24 11:25:43 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+export default {
+  components: {
+    Header
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <mt-header title="乐淘">
+  <router-link to="/" slot="left">
+    <mt-button icon="back">返回</mt-button>
+  </router-link>
+  <mt-button slot="right">搜索</mt-button>
+  </mt-header>
+</template>
